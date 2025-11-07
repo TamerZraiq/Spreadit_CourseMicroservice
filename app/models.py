148e9672bb8e6df57d62da75d3e942f80c6a3a43
David <118185346+DavidJ7705@users.noreply.github.com>
2025-11-07 18:25:45 +0000

    Updated all current functions to use new sqlite database. Needs to be changed out to docker and postgres

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
+
+class Base(DeclarativeBase):
+    pass
+
+class CourseDB(Base):
+    __tablename__ = "course"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    course_id: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    course_name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+
